bb49528b123428487789eba7d32d00ddcaeddf84
m_uellermichael <m_uellermichael@gmx.de>
2024-02-13 20:58:11 +0100

    added character + movement

---
+++ Assets/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{   
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    Vector2 velocity;
+
+    public SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetPlayerVelocity();
+    }
+
+    private void SetPlayerVelocity(){
+        velocity = Vector2.zero;
+        velocity.x = Input.GetAxisRaw("Horizontal");
+        velocity.y = Input.GetAxisRaw("Vertical");
+
+        if (velocity != Vector2.zero) 
+        {
+            float angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+            spriteRenderer.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + velocity * moveSpeed * Time.fixedDeltaTime);
+    }
+}
