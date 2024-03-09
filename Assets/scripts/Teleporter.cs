781f52a0ae3c9aaff24559729aa7d19144626c2d
m_uellermichael <m_uellermichael@gmx.de>
2024-03-09 22:40:50 +0100

    portal

---
+++ Assets/scripts/Teleporter.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public Vector2 teleportPosition;
+    public float delayBeforeTeleport = 2f;
+
+    public GameObject particlePrefab;
+    private GameObject spawnedParticles; 
+
+    private bool playerOnTeleporter = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerOnTeleporter = true;
+            Invoke("TeleportPlayer", delayBeforeTeleport);
+
+            if (particlePrefab != null)
+            {
+                spawnedParticles = Instantiate(particlePrefab, transform.position, Quaternion.identity);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerOnTeleporter = false;
+            CancelInvoke("TeleportPlayer");
+
+            if (spawnedParticles != null)
+            {
+                Destroy(spawnedParticles);
+            }
+        }
+    }
+
+    private void TeleportPlayer()
+    {
+        if (playerOnTeleporter)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if (player != null)
+            {
+                Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+                rb.velocity = Vector2.zero;
+                player.transform.position = teleportPosition;
+            }
+            else
+            {
+                Debug.LogError("Kein Player lol?? Prdn??!.");
+            }
+        }
+    }
+}
