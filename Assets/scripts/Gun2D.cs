6e9f0cb80ea7473e72b429c3e42d8523e7505b46
m_uellermichael <m_uellermichael@gmx.de>
2024-03-09 11:30:38 +0100

    shooting fixed

--- Assets/scripts/Gun2D.cs
+++ Assets/scripts/Gun2D.cs
@@ -19,16 +19,19 @@ public class Gun2D : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Vector2 playerDirection = GetPlayerDirection();
-            Debug.Log(playerDirection);
-            GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
-            bullet.GetComponent<Rigidbody2D>().velocity = playerDirection * bulletSpeed;
+            Vector2 shootDirection = GetShootDirection();
+            ShootBullet(shootDirection);
         }
     }
 
-    private Vector2 GetPlayerDirection()
+    private Vector2 GetShootDirection()
     {
-        Vector2 inputDirection = player.GetMovementInput();
+        Vector2 inputDirection = player.GetMovementInput().normalized;
+
+        if (inputDirection == Vector2.zero)
+        {
+            return Vector2.up;
+        }
 
         float horizontalInput = inputDirection.x;
         float verticalInput = inputDirection.y;
@@ -54,21 +57,21 @@ public class Gun2D : MonoBehaviour
             else if (verticalInput < 0)
             {
                 if (horizontalInput > 0)
-                    return new Vector2(1, -1); 
+                    return new Vector2(1, -1);
                 else if (horizontalInput < 0)
                     return new Vector2(-1, -1);
                 else
                     return Vector2.down;
             }
-            else
-            {
-                if (horizontalInput > 0)
-                    return Vector2.right;
-                else if (horizontalInput < 0)
-                    return Vector2.left;
-                else
-                    return Vector2.zero;
-            }
         }
+        return Vector2.zero;
+    }
+
+    private void ShootBullet(Vector2 direction)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
+        bullet.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+
+        Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), player.GetComponent<Collider2D>());
     }
 }
